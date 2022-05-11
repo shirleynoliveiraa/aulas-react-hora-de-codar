b4072752aaee8c3641ccf3a703fa6280e842a9f6
shirleynoliveiraa <shirley_nill@hotmail.com>
2022-05-11 15:04:08 -0300

    ESlint correções

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route, Link} from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 import Footer from './components/layout/Footer';
 import Navbar from './components/layout/Navbar';
 import Contato from './pages/Contato';
